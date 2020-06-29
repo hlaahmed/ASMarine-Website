aa09206a75a3646dc0c043d4817d3f538b1b0b62
InEdited <ineditedofficial@gmail.com>
2020-06-30 00:17:31 +0200

    Added working Carousel with blurred background

--- src/views/Index.js
+++ src/views/Index.js
@@ -6,6 +6,7 @@ import React from "react";
 import "assets/css/bootstrap.min.css";
 import "assets/scss/paper-kit.scss?v=1.2.0";
 import "assets/demo/demo.css?v=1.2.0";
+
 // reactstrap components
 import {
   Button,
@@ -13,6 +14,11 @@ import {
   CardBody,
   CardFooter,
   CardTitle,
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption,
   Form,
   Input,
   InputGroupAddon,
@@ -21,12 +27,15 @@ import {
   Container,
   Row,
   Col,
+  UncontrolledCarousel,
 } from "reactstrap";
 
 // core components
 import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
 import LandingPageHeader from "components/Headers/LandingPageHeader.js";
 import DemoFooter from "components/Footers/DemoFooter.js";
+import SectionCarousel from "views/index-sections/SectionCarousel.js"
+
 
 
 function LandingPage() {
@@ -69,79 +78,70 @@ function LandingPage() {
                 <h2 className="title">Video Goes here</h2>
               </Col>
             </Row>
-            {/*<Row>
+            <Row>
               <Col md="3">
                 <div className="info">
                   <div className="icon icon-info">
-                    <i className="nc-icon nc-album-2" />
+                    <h3 className="title">2019</h3>
                   </div>
                   <div className="description">
-                    <h4 className="info-title">Beautiful Gallery</h4>
+                    <h4 className="info-title">Founded</h4>
                     <p className="description">
                       Spend your time generating new ideas. You don't have to
                       think of implementing.
                     </p>
-                    <Button className="btn-link" color="info" href="#pablo">
-                      See more
-                    </Button>
                   </div>
                 </div>
               </Col>
               <Col md="3">
                 <div className="info">
                   <div className="icon icon-info">
-                    <i className="nc-icon nc-bulb-63" />
+                    <h3 className="title">2</h3>
                   </div>
                   <div className="description">
-                    <h4 className="info-title">New Ideas</h4>
+                    <h4 className="info-title">Project</h4>
                     <p>
                       Larger, yet dramatically thinner. More powerful, but
                       remarkably power efficient.
                     </p>
-                    <Button className="btn-link" color="info" href="#pablo">
-                      See more
-                    </Button>
+
                   </div>
                 </div>
               </Col>
               <Col md="3">
                 <div className="info">
                   <div className="icon icon-info">
-                    <i className="nc-icon nc-chart-bar-32" />
+                    <h3 className="title">26</h3>
                   </div>
                   <div className="description">
-                    <h4 className="info-title">Statistics</h4>
+                    <h4 className="info-title">Members</h4>
                     <p>
                       Choose from a veriety of many colors resembling sugar
                       paper pastels.
                     </p>
-                    <Button className="btn-link" color="info" href="#pablo">
-                      See more
-                    </Button>
+
                   </div>
                 </div>
               </Col>
               <Col md="3">
                 <div className="info">
                   <div className="icon icon-info">
-                    <i className="nc-icon nc-sun-fog-29" />
+                    <h3 className="title">2</h3>
                   </div>
                   <div className="description">
-                    <h4 className="info-title">Delightful design</h4>
+                    <h4 className="info-title">Publications</h4>
                     <p>
                       Find unique and handmade delightful designs related items
                       directly from our sellers.
                     </p>
-                    <Button className="btn-link" color="info" href="#pablo">
-                      See more
-                    </Button>
                   </div>
                 </div>
               </Col>
-            </Row>*/}
+            </Row>
           </Container>
         </div>
-        <div className="section section-dark text-center" style={{backgroundColor:'#0a63b6'}}>
+
+        <div className="section section-dark text-center" style={{ backgroundColor: '#0a63b6' }}>
           <Container>
             <h2 className="title">Hall Of Fame</h2>
             <Row>
@@ -163,7 +163,7 @@ function LandingPage() {
                       </div>
                     </a>
                     <p className="card-description text-center">
-                    One of the hardest aspects of the project is the integration of several domains especially the integration of Software and Hardware and getting both to work smoothly, overall i believe it is such interdisciplinary projects that impose a real challenge to us as engineers and i was more than happy to work on this project.
+                      One of the hardest aspects of the project is the integration of several domains especially the integration of Software and Hardware and getting both to work smoothly, overall i believe it is such interdisciplinary projects that impose a real challenge to us as engineers and i was more than happy to work on this project.
                     </p>
                   </CardBody>
                   <CardFooter className="text-center">
@@ -171,7 +171,7 @@ function LandingPage() {
                       className="btn-just-icon btn-neutral ml-1"
                       color="link"
                       href="https://www.linkedin.com/in/yousefmansy/"
-                      onClick={()=> window.open("https://www.linkedin.com/in/yousefmansy/", "_blank")}
+                      onClick={() => window.open("https://www.linkedin.com/in/yousefmansy/", "_blank")}
                     >
                       <i className="fa fa-linkedin" />
                     </Button>
@@ -196,18 +196,18 @@ function LandingPage() {
                       </div>
                     </a>
                     <p className="card-description text-center">
-                    It was the best experience of my life. 
-                    Meeting the teams, organizers and sponsors was amazing. I will definitely come back.
+                      It was the best experience of my life.
+                      Meeting the teams, organizers and sponsors was amazing. I will definitely come back.
                     </p>
 
-                    
+
                   </CardBody>
                   <CardFooter className="text-center">
                     <Button
                       className="btn-just-icon btn-neutral ml-1"
                       color="link"
                       href="#pablo"
-                      onClick={()=> window.open("https://www.linkedin.com/in/yousefmansy/", "_blank")}                    >
+                      onClick={() => window.open("https://www.linkedin.com/in/yousefmansy/", "_blank")}                    >
                       <i className="fa fa-linkedin" />
                     </Button>
                   </CardFooter>
@@ -231,7 +231,7 @@ function LandingPage() {
                       </div>
                     </a>
                     <p className="card-description text-center">
-                    I was really passionate about machine vision and deep learning, i believe i acquired awesome knowledge in the field and i'm still advising the team on the topic, plus RoboSub's competitive atmosphere is really awesome and it was a pleasure being part of such community                    </p>
+                      I was really passionate about machine vision and deep learning, i believe i acquired awesome knowledge in the field and i'm still advising the team on the topic, plus RoboSub's competitive atmosphere is really awesome and it was a pleasure being part of such community                    </p>
                   </CardBody>
                   <CardFooter className="text-center">
                     <Button
@@ -264,11 +264,179 @@ function LandingPage() {
                       </div>
                     </a>
                     <p className="card-description text-center">
-                    Managing a newly founded team is rather a challenging experience, but with hard-work, discipline and team work; success was the outcome.
+                      Managing a newly founded team is rather a challenging experience, but with hard-work, discipline and team work; success was the outcome.
                     </p>
                   </CardBody>
                   <CardFooter className="text-center">
-                    
+
+                    <Button
+                      className="btn-just-icon btn-neutral ml-1"
+                      color="link"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-linkedin" />
+                    </Button>
+                  </CardFooter>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+
+
+
+                <SectionCarousel></SectionCarousel>
+
+        <div className="section section-dark text-center" style={{ backgroundColor: '#0a63b6' }}>
+          <Container>
+
+
+            <Row>
+              <Col className="ml-auto mr-auto" md="8">
+                <h1 className="title">Teams</h1>
+                <h5 className="description">
+
+                </h5>
+                <br />
+                {/*<Button
+                  className="btn-round"
+                  color="info"
+                  href="#pablo"
+                  onClick={(e) => e.preventDefault()}
+                >
+                  See Details
+                </Button>*/}
+              </Col>
+            </Row>
+            <br />
+            <br />
+
+            <Row>
+              <Col md="3">
+                <Card className="card-profile card-plain">
+                  <div className="card-avatar">
+                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                      <img
+                        alt="..."
+                        src={require("assets/img/faces/Youssef.jpg")}
+                      />
+                    </a>
+                  </div>
+                  <CardBody>
+                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                      <div className="author">
+                        <CardTitle tag="h4">Yousef El Mansy</CardTitle>
+                        <h6 className="card-category">Former Software Team Leader</h6>
+                      </div>
+                    </a>
+                    <p className="card-description text-center">
+                      One of the hardest aspects of the project is the integration of several domains especially the integration of Software and Hardware and getting both to work smoothly, overall i believe it is such interdisciplinary projects that impose a real challenge to us as engineers and i was more than happy to work on this project.
+                    </p>
+                  </CardBody>
+                  <CardFooter className="text-center">
+                    <Button
+                      className="btn-just-icon btn-neutral ml-1"
+                      color="link"
+                      href="https://www.linkedin.com/in/yousefmansy/"
+                      onClick={() => window.open("https://www.linkedin.com/in/yousefmansy/", "_blank")}
+                    >
+                      <i className="fa fa-linkedin" />
+                    </Button>
+                  </CardFooter>
+                </Card>
+              </Col>
+              <Col md="3">
+                <Card className="card-profile card-plain">
+                  <div className="card-avatar">
+                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                      <img
+                        alt="..."
+                        src={require("assets/img/faces/Amr.jpg")}
+                      />
+                    </a>
+                  </div>
+                  <CardBody>
+                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                      <div className="author">
+                        <CardTitle tag="h4">Amr Abdelghany</CardTitle>
+                        <h6 className="card-category">Former Software Member</h6>
+                      </div>
+                    </a>
+                    <p className="card-description text-center">
+                      It was the best experience of my life.
+                      Meeting the teams, organizers and sponsors was amazing. I will definitely come back.
+                    </p>
+
+
+                  </CardBody>
+                  <CardFooter className="text-center">
+                    <Button
+                      className="btn-just-icon btn-neutral ml-1"
+                      color="link"
+                      href="#pablo"
+                      onClick={() => window.open("https://www.linkedin.com/in/yousefmansy/", "_blank")}                    >
+                      <i className="fa fa-linkedin" />
+                    </Button>
+                  </CardFooter>
+                </Card>
+              </Col>
+              <Col md="3">
+                <Card className="card-profile card-plain">
+                  <div className="card-avatar">
+                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                      <img
+                        alt="..."
+                        src={require("assets/img/faces/Perihane.jpg")}
+                      />
+                    </a>
+                  </div>
+                  <CardBody>
+                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                      <div className="author">
+                        <CardTitle tag="h4">Perihane Youssef</CardTitle>
+                        <h6 className="card-category">Former Software Member</h6>
+                      </div>
+                    </a>
+                    <p className="card-description text-center">
+                      I was really passionate about machine vision and deep learning, i believe i acquired awesome knowledge in the field and i'm still advising the team on the topic, plus RoboSub's competitive atmosphere is really awesome and it was a pleasure being part of such community                    </p>
+                  </CardBody>
+                  <CardFooter className="text-center">
+                    <Button
+                      className="btn-just-icon btn-neutral ml-1"
+                      color="link"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-linkedin" />
+                    </Button>
+                  </CardFooter>
+                </Card>
+              </Col>
+
+              <Col md="3">
+                <Card className="card-profile card-plain">
+                  <div className="card-avatar">
+                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                      <img
+                        alt="..."
+                        src={require("assets/img/faces/Pance3.jpg")}
+                      />
+                    </a>
+                  </div>
+                  <CardBody>
+                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                      <div className="author">
+                        <CardTitle tag="h4">Pancé Ibrahim</CardTitle>
+                        <h6 className="card-category">Former Project Manager/Business Team Leader</h6>
+                      </div>
+                    </a>
+                    <p className="card-description text-center">
+                      Managing a newly founded team is rather a challenging experience, but with hard-work, discipline and team work; success was the outcome.
+                    </p>
+                  </CardBody>
+                  <CardFooter className="text-center">
+
                     <Button
                       className="btn-just-icon btn-neutral ml-1"
                       color="link"
--- src/views/index-sections/SectionCarousel.js
+++ src/views/index-sections/SectionCarousel.js
@@ -1,21 +1,3 @@
-/*!
-
-=========================================================
-* Paper Kit React - v1.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-kit-react
-
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 
 // reactstrap components
@@ -29,27 +11,54 @@ import {
   CarouselIndicators,
   CarouselCaption,
 } from "reactstrap";
-
+import Background from "assets/img/idk2.jpg";
+var Blur = require('react-blur').default;
+var radius = "5px";
+var ydenomi;
 // core components
+var sectionStyle = {
+
+  backgroundImage: "url(" + Background + ")",
+  '-webkit-filter': 'blur(' + radius + ')',
+  'filter': 'blur(' + radius + ')',
+  'height': '100%'
+
+};
 
 const items = [
   {
-    src: require("assets/img/soroush-karimi.jpg"),
-    altText: "Somewhere",
-    caption: "Somewhere",
+    src: require("assets/img/idk3.jpg"),
+    altText: "Gamed",
+    caption: "Gamed",
   },
   {
-    src: require("assets/img/federico-beccari.jpg"),
-    altText: "Somewhere else",
-    caption: "Somewhere else",
+    src: require("assets/img/idk1.jpg"),
+    altText: "Gamed tany",
+    caption: "Gamed khalis",
   },
   {
-    src: require("assets/img/joshua-stannard.jpg"),
-    altText: "Here it is",
-    caption: "Here it is",
+    src: require("assets/img/idk2.jpg"),
+    altText: "gamed khalis",
+    caption: "Gamed tany",
   },
 ];
-
+const itemsBlurred = [
+  {
+    src: require("assets/img/idk3-blurred.jpg"),
+    altText: "Gamed",
+    caption: "Gamed",
+  },
+  {
+    src: require("assets/img/idk1-blurred.jpg"),
+    altText: "Gamed tany",
+    caption: "Gamed khalis",
+  },
+  {
+    src: require("assets/img/idk2-blurred.jpg"),
+    altText: "gamed khalis",
+    caption: "Gamed tany",
+  },
+];
 function SectionCarousel() {
   const [activeIndex, setActiveIndex] = React.useState(0);
   const [animating, setAnimating] = React.useState(false);
@@ -72,71 +81,85 @@ function SectionCarousel() {
   const goToIndex = (newIndex) => {
     if (animating) return;
     setActiveIndex(newIndex);
+    
   };
   return (
     <>
-      <div className="section pt-o" id="carousel">
-        <Container>
-          <Row>
-            <Col className="ml-auto mr-auto" md="8">
-              <Card className="page-carousel">
-                <Carousel
+
+
+      <div className="section pt-o" id="carousel" style={{
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundImage: `url(${itemsBlurred[activeIndex].src})`
+      }} >
+        <Row>
+          <Col className="ml-auto mr-auto" md="6" >
+            <Card className="page-carousel" style={{ padding: 0, margin: 0 }}>
+              <Carousel
+                activeIndex={activeIndex}
+                next={next}
+                previous={previous}
+
+              >
+                <CarouselIndicators
+                  items={items}
                   activeIndex={activeIndex}
-                  next={next}
-                  previous={previous}
+                  onClickHandler={goToIndex}
+                />
+                {items.map((item) => {
+                                    ydenomi=item.src;
+                                    console.log(items[activeIndex]);
+                  return (
+                    <CarouselItem
+                      onExiting={onExiting}
+                      onExited={onExited}
+                      key={item.src}
+                    >
+            
+                      <img src={item.src} alt={item.altText} />
+                      <CarouselCaption
+                        captionText={item.caption}
+                        captionHeader=""
+                      />
+                    </CarouselItem>
+                  );
+                })}
+                <a
+                  className="left carousel-control carousel-control-prev"
+                  data-slide="prev"
+                  href="#pablo"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    
+                    previous();
+                  }}
+                  role="button"
                 >
-                  <CarouselIndicators
-                    items={items}
-                    activeIndex={activeIndex}
-                    onClickHandler={goToIndex}
-                  />
-                  {items.map((item) => {
-                    return (
-                      <CarouselItem
-                        onExiting={onExiting}
-                        onExited={onExited}
-                        key={item.src}
-                      >
-                        <img src={item.src} alt={item.altText} />
-                        <CarouselCaption
-                          captionText={item.caption}
-                          captionHeader=""
-                        />
-                      </CarouselItem>
-                    );
-                  })}
-                  <a
-                    className="left carousel-control carousel-control-prev"
-                    data-slide="prev"
-                    href="#pablo"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      previous();
-                    }}
-                    role="button"
-                  >
-                    <span className="fa fa-angle-left" />
-                    <span className="sr-only">Previous</span>
-                  </a>
-                  <a
-                    className="right carousel-control carousel-control-next"
-                    data-slide="next"
-                    href="#pablo"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      next();
-                    }}
-                    role="button"
-                  >
-                    <span className="fa fa-angle-right" />
-                    <span className="sr-only">Next</span>
-                  </a>
-                </Carousel>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      </div>{" "}
+                  <span className="fa fa-angle-left" />
+                  <span className="sr-only">Previous</span>
+                </a>
+                <a
+                  className="right carousel-control carousel-control-next"
+                  data-slide="next"
+                  href="#pablo"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    next();
+                  }}
+                  role="button"
+                >
+                  <span className="fa fa-angle-right" />
+                  <span className="sr-only">Next</span>
+                </a>
+              </Carousel>
+            </Card>
+          </Col>
+        </Row>
+
+      </div>
+
+      {" "}
     </>
   );
 }
